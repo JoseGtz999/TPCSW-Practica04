48c208216dae1229e59ccdcdd6112f180a51950e
jorgeaveraf <jorgeaveraf@gmail.com>
2024-11-10 14:04:30 -0600

    Addition for department's crud

---
+++ src/main/java/com/example/demo/RepositoryDepartamentos.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryDepartamentos extends JpaRepository<Departamento, Long> {
+    
+}
